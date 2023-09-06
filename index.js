b0c1f7e37f1c96a43aef1ed6472be46ccd1b59f4
waelalsabbah <alsabbahwael@gmail.com>
2023-09-06 09:32:59 +0200

    the change is done

---
+++ index.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor'; // import the script
+
+const myColor = randomColor({
+  luminosity: 'random',
+  hue: 'random',
+});
+console.log(myColor);
+console.log(chalk.hex(myColor).bold('#999'));
+console.log(chalk.hex(myColor).bold('###############################'));
+console.log(chalk.hex(myColor).bold('###############################'));
+console.log(chalk.hex(myColor).bold('###############################'));
+console.log(chalk.hex(myColor).bold('###############################'));
+console.log(chalk.hex(myColor).bold('#######               #########'));
+console.log(chalk.hex(myColor).bold('#######               #########'));
+console.log(chalk.hex(myColor).bold('#######               #########'));
+console.log(chalk.hex(myColor).bold('#######               #########'));
+console.log(chalk.hex(myColor).bold('#######               #########'));
+console.log(chalk.hex(myColor).bold('###############################'));
+console.log(chalk.hex(myColor).bold('###############################'));
